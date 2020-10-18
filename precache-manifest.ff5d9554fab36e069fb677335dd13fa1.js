c8e8f0159fb0e4eeb4dbdc3f5c23f95d3d5675fb
unknown <tpfpal486@naver.com>
2020-10-18 22:44:20 +0900

    Updates

---
+++ precache-manifest.ff5d9554fab36e069fb677335dd13fa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81c45c655bcf9673e21f35ad8ab1612",
+    "url": "/tarrotquiz/index.html"
+  },
+  {
+    "revision": "b978a5d6e2773857ee26",
+    "url": "/tarrotquiz/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e71d3f3bd865eea6342d",
+    "url": "/tarrotquiz/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "b978a5d6e2773857ee26",
+    "url": "/tarrotquiz/static/js/2.bab2caa1.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/tarrotquiz/static/js/2.bab2caa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71d3f3bd865eea6342d",
+    "url": "/tarrotquiz/static/js/main.6d57ef6b.chunk.js"
+  },
+  {
+    "revision": "de5eeb496849ad7764e0",
+    "url": "/tarrotquiz/static/js/runtime-main.97561400.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/tarrotquiz/static/media/logo.ee7cd8ed.svg"
+  }
+]);
